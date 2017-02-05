4b961af821a50676a5357eb4284f1dbc42eac7bf
“Richard Middleton” <Richard@richardmiddleton.me>
2017-02-05 21:51:49 +0100

    Created localStorage

--- main.js
+++ main.js
@@ -10,12 +10,44 @@ function saveBookmark(e) {
     var siteUrl = document.getElementById('siteUrl').value;
 
     var bookmark = {
-      name: siteName,
-      url: siteUrl
+        name: siteName,
+        url: siteUrl
     }
 
-    console.log(bookmark);
 
+    /* // Local storage test
+
+    localStorage.setItem('test', 'hello world');
+
+    console.log(localStorage.getItem('test'));
+
+    localStorage.removeItem('test');
+
+    console.log(localStorage.getItem('test'));
+
+    */
+
+    // Test if bookmark is null
+    if (localStorage.getItem('bookmarks') === null) {
+
+      // Init array
+
+      var bookmarks = [];
+      // Add to array
+      bookmarks.push(bookmark);
+      // Set it to localStorage
+      localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+
+    }
+
+    else {
+      // Get bookmarks from localStorage
+        var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+        // Add bookmark to array
+        bookmarks.push(bookmark);
+        // Reset back to localStorage
+        localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+    }
     // prevent form from submitting
     e.preventDefault();
 
